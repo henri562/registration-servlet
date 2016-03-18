1d4acdabe252be05fea9eb919400a12d1bff9f17
Henry Lin <linm@mail.npu.edu>
2016-03-17 19:16:31 -0700

    conform to Servlet 3.0 deployment annotation

--- src/java/myservlet/RegistrationForm.java
+++ src/java/myservlet/RegistrationForm.java
@@ -5,10 +5,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/register")
 public class RegistrationForm extends HttpServlet {
     private static final String CONTENT_TYPE = "text/html";
 
